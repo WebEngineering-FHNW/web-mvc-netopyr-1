1624b09260f61e1f863bd0f2aea0f451de50e9cf
Michael Heinrichs <netopyr@users.noreply.github.com>
2020-10-04 21:45:49 +0200

    Solution for assignment 4

--- src/main/java/ch/fhnw/webec/converter/ConverterController.java
+++ src/main/java/ch/fhnw/webec/converter/ConverterController.java
@@ -12,12 +12,15 @@ import java.util.Map;
 public class ConverterController {
 
     @GetMapping("convert")
-    public ModelAndView convert(@RequestParam double inches) {
-        final double result = inches * 2.54;
+    public ModelAndView convert(@RequestParam(required = false) Double feet, @RequestParam(required = false) Double inches) {
+        final double feetValue = feet != null? feet : 0;
+        final double inchesValue = inches != null? inches : 0;
+        final double result = (12 * feetValue + inchesValue) * 2.54;
         final int cm = (int) result;
         final int mm = (int)Math.round(10 * (result - cm));
         final Map<String, Object> model = new HashMap<>();
-        model.put("inches", inches);
+        model.put("feet", feetValue);
+        model.put("inches", inchesValue);
         model.put("cm", cm);
         model.put("mm", mm);
         return new ModelAndView("ConverterOutput", model);
--- src/test/java/ch/fhnw/webec/converter/ConverterControllerTest.java
+++ src/test/java/ch/fhnw/webec/converter/ConverterControllerTest.java
@@ -5,19 +5,71 @@ import org.springframework.web.servlet.ModelAndView;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ConverterControllerTest {
+class ConverterControllerTest {
 
     @Test
-    public void testSimpleConversion() {
+    void testInchesConversion() {
         // given:
         final ConverterController controller = new ConverterController();
 
         // when:
-        final ModelAndView result = controller.convert(5);
+        final ModelAndView result = controller.convert(0.0, 5.0);
 
         // then:
         assertThat(result.getModel()).containsEntry("cm", 12);
         assertThat(result.getModel()).containsEntry("mm", 7);
+    }
+
+    @Test
+    void testFeetConversion() {
+        // given:
+        final ConverterController controller = new ConverterController();
+
+        // when:
+        final ModelAndView result = controller.convert(11.0, 0.0);
+
+        // then:
+        assertThat(result.getModel()).containsEntry("cm", 335);
+        assertThat(result.getModel()).containsEntry("mm", 3);
+    }
+
+    @Test
+    void testFeetAndInchesConversion() {
+        // given:
+        final ConverterController controller = new ConverterController();
+
+        // when:  91.44
+        final ModelAndView result = controller.convert(3.0, 7.0);
+
+        // then:
+        assertThat(result.getModel()).containsEntry("cm", 109);
+        assertThat(result.getModel()).containsEntry("mm", 2);
+    }
+
+    @Test
+    void testFeetNullConversion() {
+        // given:
+        final ConverterController controller = new ConverterController();
+
+        // when:
+        final ModelAndView result = controller.convert(null, 5.0);
+
+        // then:
+        assertThat(result.getModel()).containsEntry("cm", 12);
+        assertThat(result.getModel()).containsEntry("mm", 7);
+    }
+
+    @Test
+    void testInchesNullConversion() {
+        // given:
+        final ConverterController controller = new ConverterController();
+
+        // when:
+        final ModelAndView result = controller.convert(11.0, null);
+
+        // then:
+        assertThat(result.getModel()).containsEntry("cm", 335);
+        assertThat(result.getModel()).containsEntry("mm", 3);
     }
 
 }
